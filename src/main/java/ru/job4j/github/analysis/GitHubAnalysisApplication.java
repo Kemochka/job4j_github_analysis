3733678c21bdbbe282ffb5f08a6742ab8fe5e77d
kemochka <kemaevak@yandex.ru>
2024-12-03 17:08:09 +0300

    first commit

---
+++ src/main/java/ru/job4j/github/analysis/GitHubAnalysisApplication.java
@@ -0,0 +1,11 @@
+package ru.job4j.github.analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubAnalysisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubAnalysisApplication.class, args);
+    }
+}
